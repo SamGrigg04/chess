434d9352e533046dd027b109c284cba8d4d4a8ed
Sam Grigg <mrsaaagr@gmail.com>
2026-01-20 22:23:10 -0700

    King done!

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -8,14 +8,14 @@ public class KingMovesCalculator extends MoveCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition){
         Collection<ChessMove> possibleMoves = new ArrayList<>();
-//        possibleMoves.addAll(upMove(board, myPosition));
-//        possibleMoves.addAll(downMove(board, myPosition));
-//        possibleMoves.addAll(rightMove(board, myPosition));
-//        possibleMoves.addAll(leftMove(board, myPosition));
-//        possibleMoves.addAll(upRightMove(board, myPosition));
-//        possibleMoves.addAll(upLeftMove(board, myPosition));
-//        possibleMoves.addAll(downRightMove(board, myPosition));
-//        possibleMoves.addAll(downLeftMove(board, myPosition));
+        possibleMoves.addAll(slide(this::upMove, board, myPosition, true));
+        possibleMoves.addAll(slide(this::downMove, board, myPosition, true));
+        possibleMoves.addAll(slide(this::leftMove, board, myPosition, true));
+        possibleMoves.addAll(slide(this::rightMove, board, myPosition, true));
+        possibleMoves.addAll(slide(this::upRightMove, board, myPosition, true));
+        possibleMoves.addAll(slide(this::upLeftMove, board, myPosition, true));
+        possibleMoves.addAll(slide(this::downRightMove, board, myPosition, true));
+        possibleMoves.addAll(slide(this::downLeftMove, board, myPosition, true));
         return possibleMoves;
     }
 }
