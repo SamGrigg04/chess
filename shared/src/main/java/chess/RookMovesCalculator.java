86bc9861cfc2afe8712b447388a700c91f1fe7ab
Sam Grigg <mrsaaagr@gmail.com>
2026-01-20 22:23:23 -0700

    Rook done!

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -7,10 +7,10 @@ public class RookMovesCalculator extends MoveCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition){
         Collection<ChessMove> possibleMoves = new ArrayList<>();
-        possibleMoves.addAll(slide(this::upMove, board, myPosition));
-//        possibleMoves.addAll(slide(this::downMove, board, myPosition));
-//        possibleMoves.addAll(slide(this::rightMove, board, myPosition));
-//        possibleMoves.addAll(slide(this::leftMove, board, myPosition));
+        possibleMoves.addAll(slide(this::upMove, board, myPosition, false));
+        possibleMoves.addAll(slide(this::downMove, board, myPosition, false));
+        possibleMoves.addAll(slide(this::leftMove, board, myPosition, false));
+        possibleMoves.addAll(slide(this::rightMove, board, myPosition, false));
         return possibleMoves;
     }
 }
