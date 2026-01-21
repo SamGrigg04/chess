39bbc758006378422297d4c7a0b38b9ae022bfaa
Sam Grigg <mrsaaagr@gmail.com>
2026-01-20 22:23:04 -0700

    Bishop done!

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -7,10 +7,10 @@ public class BishopMovesCalculator extends MoveCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition){
         Collection<ChessMove> possibleMoves = new ArrayList<>();
-//        possibleMoves.addAll(slide(this::upRightMove, board, myPosition));
-//        possibleMoves.addAll(slide(this::downRightMove, board, myPosition));
-//        possibleMoves.addAll(slide(this::upLeftMove, board, myPosition));
-//        possibleMoves.addAll(slide(this::downLeftMove, board, myPosition));
+        possibleMoves.addAll(slide(this::upRightMove, board, myPosition, false));
+        possibleMoves.addAll(slide(this::upLeftMove, board, myPosition, false));
+        possibleMoves.addAll(slide(this::downRightMove, board, myPosition, false));
+        possibleMoves.addAll(slide(this::downLeftMove, board, myPosition, false));
         return possibleMoves;
     }
 }
