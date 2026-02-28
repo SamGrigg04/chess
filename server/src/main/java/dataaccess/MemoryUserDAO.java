14d73b31b9baf4d1e2dc6fe07309b4ca98926a5b
Sam Grigg <mrsaaagr@gmail.com>
2026-02-28 12:34:54 -0700

    Implemented createUser and getUser

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,20 +1,28 @@
 package dataaccess;
 
 import model.UserData;
+import service.AlreadyTakenException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class MemoryUserDAO implements UserDAO {
     private final Map<String, UserData> users = new HashMap<>();
 
     @Override
-    public void createUser(String username, String password, String email) throws DataAccessException {
-
+    public void createUser(String username, String password, String email) throws AlreadyTakenException {
+        UserData newUser = new UserData(username, password, email);
+        users.put(username, newUser);
     }
 
     @Override
-    public UserData getUser(String username) throws DataAccessException {
+    public UserData getUser(String username) throws DataAccessException  {
+        for (String key : users.keySet()) {
+            if (Objects.equals(users.get(key).username(), username)) {
+                return users.get(key);
+            }
+        }
         return null;
     }
 
