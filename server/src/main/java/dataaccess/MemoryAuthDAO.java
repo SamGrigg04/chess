5055f09c9c84cce46f9a9229fe4653876e039a30
Sam Grigg <mrsaaagr@gmail.com>
2026-02-28 12:33:19 -0700

    Implemented createAuth and getAuth

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -4,17 +4,24 @@ import model.AuthData;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class MemoryAuthDAO implements AuthDAO {
     private final Map<String, AuthData> authTokens = new HashMap<>();
 
     @Override
     public void createAuth(String authToken, String username) throws DataAccessException {
-
+        AuthData newAuth = new AuthData(authToken, username);
+        authTokens.put(authToken, newAuth);
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        for (String key : authTokens.keySet()) {
+            if (Objects.equals(authTokens.get(key).authToken(), authToken)) {
+                return authTokens.get(key);
+            }
+        }
         return null;
     }
 
