2b44ae3de334d934813390a88c64508937cc420b
Sam Grigg <mrsaaagr@gmail.com>
2026-02-27 18:19:40 -0700

    registered endpoints and put a generic exception handler in

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,17 +1,40 @@
 package server;
 
 import io.javalin.*;
+import handler.ClearHandler;
+import handler.GameHandler;
+import handler.UserHandler;
 
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
+        var userHandler = new UserHandler();
+        var gameHandler = new GameHandler();
+        var clearHandler = new ClearHandler();
+
+
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
+        javalin.post("/user", userHandler::register);
+        javalin.post("/session", userHandler::login);
+        javalin.delete("/session", userHandler::logout);
+
+        javalin.get("/game", gameHandler::listGames);
+        javalin.post("/game", gameHandler::createGame);
+        javalin.put("/game", gameHandler::joinGame);
+
+        javalin.delete("/db", clearHandler::clear);
+
+        javalin.exception(Exception.class, (e, ctx) ->
+                ctx.status(500).json(new ErrorResponse("Error: " + e.getMessage())));
 
         // Register your endpoints and exception handlers here.
 
     }
+
+    private record ErrorResponse(String message) {}
 
     public int run(int desiredPort) {
         javalin.start(desiredPort);
