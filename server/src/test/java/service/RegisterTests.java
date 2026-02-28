9a3e4b270cfcdd4a1d6a8fefaa362f2c48a2b522
Sam Grigg <mrsaaagr@gmail.com>
2026-02-28 13:12:10 -0700

    Thanks, CS 203

---
+++ server/src/test/java/service/RegisterTests.java
@@ -0,0 +1,42 @@
+package service;
+
+import Request.RegisterRequest;
+import Result.AuthResult;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RegisterTests {
+
+    @Test
+    void registerSuccess() throws Exception {
+        var authDAO = new MemoryAuthDAO();
+        var gameDAO = new MemoryGameDAO();
+        var userDAO = new MemoryUserDAO();
+
+        var userService = new UserService(authDAO, gameDAO, userDAO);
+
+        AuthResult result = userService.register(new RegisterRequest("bob", "psw", "b@mail.com"));
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals("bob", result.username());
+        Assertions.assertNotNull(result.authToken());
+        Assertions.assertNotNull(userDAO.getUser("bob"));
+        Assertions.assertNotNull(authDAO.getAuth(result.authToken()));
+    }
+
+    @Test
+    void usernameAlreadyTaken() throws Exception {
+        var authDAO = new MemoryAuthDAO();
+        var gameDAO = new MemoryGameDAO();
+        var userDAO = new MemoryUserDAO();
+
+        var userService = new UserService(authDAO, gameDAO, userDAO);
+        userService.register(new RegisterRequest("taken", "pw", "t@mail.com"));
+
+        Assertions.assertThrows(AlreadyTakenException.class,
+                () -> userService.register(new RegisterRequest("taken", "wp", "u@mail.com")));
+    }
+}
