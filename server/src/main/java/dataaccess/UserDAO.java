78767b38b8c6ce4efebec1a19d99bf898804a506
Sam Grigg <mrsaaagr@gmail.com>
2026-03-09 17:51:26 -0600

    updated for the SQL child

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -4,7 +4,7 @@ import model.UserData;
 import service.AlreadyTakenException;
 
 public interface UserDAO {
-    void createUser(String username, String password, String email) throws AlreadyTakenException;
+    void createUser(String username, String password, String email) throws AlreadyTakenException, DataAccessException;
     UserData getUser(String username) throws DataAccessException;
     void clear();
 }
