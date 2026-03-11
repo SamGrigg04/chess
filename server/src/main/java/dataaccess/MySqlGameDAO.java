250ede76e7a55a4c4210ffe8ba06397ca6c61d71
Sam Grigg <mrsaaagr@gmail.com>
2026-03-11 11:05:07 -0600

    goodbye, unused import

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -4,7 +4,6 @@ import chess.ChessGame;
 import com.google.gson.Gson;
 import model.GameData;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
