acf28ae04604a236107f52dd6b06bfa78177ff9f
Sam Grigg <mrsaaagr@gmail.com>
2026-03-10 22:00:32 -0600

    updated to handle errors

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 public interface GameDAO {
     Integer createGame(String gameName) throws DataAccessException;
     GameData getGame(Integer gameID) throws DataAccessException;
-    Collection<GameData> listGames();
+    Collection<GameData> listGames() throws DataAccessException;
     void updateGame(Integer gameID, String playerColor, String username) throws DataAccessException;
     void clear() throws DataAccessException;
 }
