0a3559e3a83be4be6db8982597367841570df272
Sam Grigg <mrsaaagr@gmail.com>
2026-03-24 12:04:39 -0600

    well, I forgot to add this to the git when I made it, but basically it was just copying what I had for the individual chess boards and modifying it so that depending on the player color it displays differently. easier than I expected

---
+++ client/src/main/java/ui/ChessBoardRenderer.java
@@ -0,0 +1,184 @@
+package ui;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static ui.EscapeSequences.*;
+
+public final class ChessBoardRenderer {
+
+    private static final int BOARD_SIZE_IN_SQUARES = 10;
+    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 1;
+    private static final String EMPTY = "   ";
+
+    public static void render(PlayerColor playerColor) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        render(out, playerColor.config());
+    }
+
+    private static void render(PrintStream out, ChessBoardConfig config) {
+        // clears the screen
+        out.print(ERASE_SCREEN);
+
+        // prints the headers at the top, then the board spaces (with the side headers), then the bottom headers
+        drawHeaders(out, config.columnHeaders());
+        drawChessBoard(out, config);
+        drawHeaders(out, config.columnHeaders());
+
+        // Sets colors generally for the whole UI
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+
+    private static void drawHeaders(PrintStream out, String[] headers) {
+        setBlack(out);
+
+        for (String header : headers) {
+            drawHeader(out, header);
+        }
+
+        out.println();
+    }
+
+    private static void drawHeader(PrintStream out, String headerText) {
+        int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+        int suffixLength = 0;
+
+        out.print(EMPTY.repeat(prefixLength));
+        printHeaderText(out, headerText);
+        out.print(EMPTY.repeat(suffixLength));
+    }
+
+    private static void printHeaderText(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(SET_TEXT_COLOR_BLACK);
+
+        out.print(player);
+
+        setBlack(out);
+    }
+
+    private static void drawChessBoard(PrintStream out, ChessBoardConfig config) {
+        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES - 2; ++boardRow) {
+            String[] rowContents = config.initialBoard()[boardRow];
+            String rowHeader = config.rowHeaders()[boardRow];
+            drawRowOfSquares(out, boardRow, rowContents, rowHeader, config.topPieceTextColor(), config.bottomPieceTextColor());
+        }
+    }
+
+    private static void drawRowOfSquares(PrintStream out,
+                                         int boardRow,
+                                         String[] rowContents,
+                                         String rowHeader,
+                                         String topPieceTextColor,
+                                         String bottomPieceTextColor) {
+
+        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
+            setLightGrey(out);
+
+            drawHeader(out, rowHeader);
+
+            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES - 2; ++boardCol) {
+                boolean useWhiteSquare = (boardRow % 2 == 0) == (boardCol % 2 == 0);
+
+                if (useWhiteSquare) {
+                    setWhite(out);
+                } else {
+                    setBlack(out);
+                }
+
+                int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+                int suffixLength = 0;
+
+                out.print(EMPTY.repeat(prefixLength));
+                if (boardRow <= 1) {
+                    out.print(topPieceTextColor);
+                }
+                if (boardRow >= 6) {
+                    out.print(bottomPieceTextColor);
+                }
+                out.print(rowContents[boardCol]);
+                out.print(EMPTY.repeat(suffixLength));
+            }
+
+            drawHeader(out, rowHeader);
+
+            out.println();
+        }
+    }
+
+    private static void setWhite(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLACK);
+    }
+
+    private static void setLightGrey(PrintStream out) {
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(SET_TEXT_COLOR_BLACK);
+    }
+
+    public record ChessBoardConfig(
+            String[] columnHeaders,
+            String[] rowHeaders,
+            String[][] initialBoard,
+            String topPieceTextColor,
+            String bottomPieceTextColor
+    ) {
+    }
+
+    public enum PlayerColor {
+        BLACK(
+                new ChessBoardConfig(
+                        new String[]{EMPTY, " h ", " g ", " f ", " e ", " d ", " c ", " b ", " a ", EMPTY},
+                        new String[]{" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "},
+                        new String[][]{
+                                {" r ", " n ", " b ", " q ", " k ", " b ", " n ", " r "},
+                                {" p ", " p ", " p ", " p ", " p ", " p ", " p ", " p "},
+                                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                                {" P ", " P ", " P ", " P ", " P ", " P ", " P ", " P "},
+                                {" R ", " N ", " B ", " Q ", " K ", " B ", " N ", " R "}
+                        },
+                        SET_TEXT_COLOR_RED,
+                        SET_TEXT_COLOR_BLUE
+                )
+        ),
+        WHITE(
+                new ChessBoardConfig(
+                        new String[]{EMPTY, " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h ", EMPTY},
+                        new String[]{" 8 ", " 7 ", " 6 ", " 5 ", " 4 ", " 3 ", " 2 ", " 1 "},
+                        new String[][]{
+                                {" R ", " N ", " B ", " Q ", " K ", " B ", " N ", " R "},
+                                {" P ", " P ", " P ", " P ", " P ", " P ", " P ", " P "},
+                                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                                {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+                                {" p ", " p ", " p ", " p ", " p ", " p ", " p ", " p "},
+                                {" r ", " n ", " b ", " q ", " k ", " b ", " n ", " r "}
+                        },
+                        SET_TEXT_COLOR_BLUE,
+                        SET_TEXT_COLOR_RED
+                )
+        );
+
+        private final ChessBoardConfig config;
+
+        PlayerColor(ChessBoardConfig config) {
+            this.config = config;
+        }
+
+        ChessBoardConfig config() {
+            return config;
+        }
+    }
+
+}
