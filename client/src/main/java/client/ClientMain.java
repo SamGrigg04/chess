df57ef8d7500b991e48025db77e39c0c61939c06
Sam Grigg <mrsaaagr@gmail.com>
2026-03-24 09:54:32 -0600

    Code from pet shop

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,12 +1,20 @@
 package client;
 
-import chess.*;
+import client.Client;
 
 public class ClientMain {
     public static void main(String[] args) {
-        int port = 8080; // or whatever your server runs on
+        String serverUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
 
-        var client = new Client(port);
-        client.run();
+        try {
+            new Client(serverUrl).run();
+
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
     }
+
 }
