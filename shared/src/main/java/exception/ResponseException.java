fb5d49c03179d1cb9afcac3053b0b41e81217c23
Sam Grigg <mrsaaagr@gmail.com>
2026-03-25 13:57:08 -0600

    added codes for different errors

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -1,15 +1,12 @@
 package exception;
 
-import com.google.gson.Gson;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class ResponseException extends Exception {
 
     public enum Code {
         ServerError,
         ClientError,
+        AlreadyTakenError,
+        UnauthorizedError
     }
 
     final private Code code;
@@ -17,17 +14,6 @@ public class ResponseException extends Exception {
     public ResponseException(Code code, String message) {
         super(message);
         this.code = code;
-    }
-
-    public String toJson() {
-        return new Gson().toJson(Map.of("message", getMessage(), "status", code));
-    }
-
-    public static ResponseException fromJson(String json) {
-        var map = new Gson().fromJson(json, HashMap.class);
-        var status = Code.valueOf(map.get("status").toString());
-        String message = map.get("message").toString();
-        return new ResponseException(status, message);
     }
 
     public Code code() {
@@ -38,6 +24,8 @@ public class ResponseException extends Exception {
         return switch (httpStatusCode) {
             case 500 -> Code.ServerError;
             case 400 -> Code.ClientError;
+            case 403 -> Code.AlreadyTakenError;
+            case 401 -> Code.UnauthorizedError;
             default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
         };
     }
@@ -46,6 +34,8 @@ public class ResponseException extends Exception {
         return switch (code) {
             case ServerError -> 500;
             case ClientError -> 400;
+            case AlreadyTakenError -> 403;
+            case UnauthorizedError -> 401;
         };
     }
 }
