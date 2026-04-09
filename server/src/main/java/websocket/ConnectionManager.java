fed7f1b107239f5cbd476e2a72cd53089db38793
Sam Grigg <mrsaaagr@gmail.com>
2026-04-09 12:31:46 -0600

    So I think this one is done. But I thought that a few hours ago too

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -2,9 +2,6 @@ package websocket;
 
 import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
-import websocket.commands.UserGameCommand;
-import websocket.messages.LoadGameMessage;
-import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 import websocket.messages.ServerMessage.*;
 
@@ -14,66 +11,58 @@ import java.util.HashMap;
 
 public class ConnectionManager {
     public final HashMap<Integer, ArrayList<Session>> connections = new HashMap<>();
-    public final ArrayList<Session> sessions = new ArrayList<>();
     public final Gson gson = new Gson();
 
+    // Adds a session to the list by gameID. If the gameID doesn't exist, creates a new list for it.
     public void add(Integer gameID, Session session) {
         ArrayList<Session> sessions = connections.get(gameID);
+        if (sessions == null) {
+            sessions = new ArrayList<>();
+        }
         sessions.add(session);
         connections.put(gameID, sessions);
     }
 
+    // Removes a session from the list by gameID. If that list is now empty, it removes the connection.
+    // So when the last player/observer leaves, the connection will automatically be deleted
     public void remove(Integer gameID, Session session) {
-        // using the wrong list?
-        sessions.remove(session);
-        connections.remove(gameID);
+        ArrayList<Session> sessions = connections.get(gameID);
+        if (sessions != null) {
+            sessions.remove(session);
+            if (sessions.isEmpty()) {
+                connections.remove(gameID);
+            }
+        }
     }
 
+    // Sends a serialized message to one user (usually root)
     public void sendToOne(Session session, ServerMessage message) throws IOException {
         if (session.isOpen()) {
-            session.getRemote().sendString(new Gson().toJson(message.toString()));
+            session.getRemote().sendString(gson.toJson(message));
         }
     }
 
-    public void sendToOthers(Integer gameID, Session session, ServerMessage message) {
-
-    }
-
-    public void sendToGame(Integer gameID, Session session, ServerMessage message) {
-
-    }
-
-    //TODO: split to three methods
-    public void broadcast(Session rootSession, ServerMessage notification, UserGameCommand command) throws IOException {
-        String msg = notification.toString();
-
-        if (command.getCommandType() == UserGameCommand.CommandType.CONNECT) {
-            //TODO: connect sends LOAD_GAME to the sender and Notification to the other clients
-            ArrayList<Session> sessions = connections.get(command.getGameID()); // could be null on first add
+    // Sends a serialized message to all users except the one passed in that share a game with root
+    public void sendToOthers(Integer gameID, Session session, ServerMessage message) throws IOException {
+        ArrayList<Session> sessions = connections.get(gameID);
+        if (sessions != null) {
             for (Session c : sessions) {
-                if (c.isOpen()) {
-                    if (!c.equals(rootSession)) {
-                        c.getRemote().sendString(new NotificationMessage(ServerMessageType.NOTIFICATION).toString()); //needs to be cereal
-                    } else {
-                        c.getRemote().sendString(new Gson().toJson(msg)); // needs to be cereal
-                    }
+                if (c.isOpen() && !c.equals(session)) {
+                    c.getRemote().sendString(gson.toJson(message));
                 }
             }
-        } else if (command.getCommandType() == UserGameCommand.CommandType.LEAVE) {
-            //TODO: leave sends a Notification to all other clients
-
-        } else if (command.getCommandType() == UserGameCommand.CommandType.RESIGN) {
-            //TODO: resign sends a Notification to all clients
-
-        } else if (command.getCommandType() == UserGameCommand.CommandType.MAKE_MOVE) {
-            //TODO: makeMove sends LOAD_GAME to all clients with an updated game
-
-        } else {
-            //TODO: Send an Error to the root
         }
-
     }
 
-
-
+    // Sends a serialized message to all users in a game with root (including root)
+    public void sendToGame(Integer gameID, ServerMessage message) throws IOException {
+        ArrayList<Session> sessions = connections.get(gameID);
+        if (sessions != null) {
+            for (Session c : sessions) {
+                if (c.isOpen()) {
+                    c.getRemote().sendString(gson.toJson(message));
+                }
+            }
+        }
+    }
 }
