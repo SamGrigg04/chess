75011ba1423a892840f42a8089e0ea9c61275d2f
Sam Grigg <mrsaaagr@gmail.com>
2026-04-09 13:18:55 -0600

    Hello, phase 0. How would you like a new method?

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -225,6 +225,12 @@ public class ChessGame {
         return true;
     }
 
+    public boolean isGameOver() {
+        return isInCheckmate(TeamColor.WHITE)
+                || isInCheckmate(TeamColor.BLACK)
+                || isInStalemate(TeamColor.WHITE)
+                || isInStalemate(TeamColor.BLACK);
+    }
 
     private Collection<ChessMove> checkAllTeamMoves(ChessBoard board, TeamColor teamColor) {
         Collection<ChessMove> allValidMoves = new ArrayList<>();
