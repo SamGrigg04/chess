8b37d1a8bbf78c409501a6589ef4b8a4ee90c1dc
Sam Grigg <mrsaaagr@gmail.com>
2026-04-08 18:29:31 -0600

    Here's the enum with better names

--- client/src/main/java/client/ClientSession.java
+++ client/src/main/java/client/ClientSession.java
@@ -1,19 +1,72 @@
 package client;
 
+import model.GameData;
+import ui.BoardPerspective;
+
 public class ClientSession {
+    private String visitorName;
+    private String authToken;
+    private State state = State.SIGNEDOUT;
+    private Integer activeGameID;
+    private BoardPerspective activePerspective;
+    private boolean observer;
+    private GameData currentGame;
 
-    public void signIn() {}
+    public void signIn(String username, String token) {
+        visitorName = username;
+        authToken = token;
+        state = State.SIGNEDIN;
+    }
 
-    public void signOut() {}
-
+    public void signOut() {
+        visitorName = null;
+        authToken = null;
+        state = State.SIGNEDOUT;
+        leaveGame();
+    }
     public void startGame() {}
 
-    public void leaveGame() {}
-
+    public void leaveGame() {
+        activeGameID = null;
+        activePerspective = null;
+        observer = false;
+        currentGame = null;
+        if (state != State.SIGNEDOUT) {
+            state = State.SIGNEDIN;
+        }
+    }
     public void updateGame() {}
 
     public void assertSignedIn() {}
 
     public void assertPlaying() {}
+
+    public String getVisitorName() {
+        return visitorName;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public Integer getActiveGameID() {
+        return activeGameID;
+    }
+
+    public BoardPerspective getActivePerspective() {
+        return activePerspective;
+    }
+
+    public boolean isObserver() {
+        return observer;
+    }
+
+    public GameData getCurrentGame() {
+        return currentGame;
+    }
 
 }
