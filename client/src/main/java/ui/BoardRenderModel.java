f9ef40138e4b71f66c7b8cb460301f01ae2b6672
Sam Grigg <mrsaaagr@gmail.com>
2026-04-08 18:31:08 -0600

    Here's the little baby record all grown up with its own place

---
+++ client/src/main/java/ui/BoardRenderModel.java
@@ -0,0 +1,17 @@
+package ui;
+
+import chess.ChessBoard;
+import chess.ChessPosition;
+
+import java.util.Collection;
+
+public record BoardRenderModel(
+        String[] columnHeaders,
+        String[] rowHeaders,
+        ChessBoard board,
+        ChessPosition startPosition,
+        Collection<ChessPosition> endPositions,
+        String yourPieceTextColor,
+        String opposingPieceTextColor
+) {
+}
