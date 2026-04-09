af923fcbe96756e2f6f84481ea26929b94f5cdbd
Sam Grigg <mrsaaagr@gmail.com>
2026-04-09 00:49:17 -0600

    figuring it out. One small step for me, and then another, and then another until it is done

--- server/src/main/java/websocket/WebSocketService.java
+++ server/src/main/java/websocket/WebSocketService.java
@@ -1,8 +1,40 @@
 package websocket;
 
+import chess.ChessGame;
+import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
+import exception.ResponseException;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.UserGameCommand;
+import websocket.result.ConnectResult;
 
 public class WebSocketService {
+    private final GameDAO gameDAO;
 
+    public WebSocketService(GameDAO gameDAO) {
+        this.gameDAO = gameDAO;
+    }
 
+    public ConnectResult connect(UserGameCommand command) throws ResponseException, DataAccessException {
+        if (!validateAuth(command.getAuthToken())) {
+            throw new ResponseException("Error: unauthorized");
+        }
+        if (!validateGame(command.getGameID())) {
+            throw new ResponseException("Error: bad request");
+        }
+
+        ChessGame game = gameDAO.getGame(command.getGameID()).game();
+
+        String notificationText = ""; //TODO
+
+        return new ConnectResult(game, notificationText);
+    }
+
+    public boolean validateAuth(String AuthToken) {
+        return false;
+    }
+
+    public boolean validateGame(Integer gameID) {
+        return false;
+    }
 }
