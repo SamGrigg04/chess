468f041fe2c73c4bdfabece0442444cb915e0ac9
Sam Grigg <mrsaaagr@gmail.com>
2026-04-08 18:21:31 -0600

    May need more, may need less

--- client/src/main/java/client/ClientSession.java
+++ client/src/main/java/client/ClientSession.java
@@ -1,4 +1,19 @@
 package client;
 
 public class ClientSession {
+
+    public void signIn() {}
+
+    public void signOut() {}
+
+    public void startGame() {}
+
+    public void leaveGame() {}
+
+    public void updateGame() {}
+
+    public void assertSignedIn() {}
+
+    public void assertPlaying() {}
+
 }
