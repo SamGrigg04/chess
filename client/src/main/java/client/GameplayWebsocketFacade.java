37d9ab2b7b1f38d4b6c4757760ff662410204b7a
Sam Grigg <mrsaaagr@gmail.com>
2026-04-08 19:21:59 -0600

    stubbed the class

--- client/src/main/java/client/GameplayWebsocketFacade.java
+++ client/src/main/java/client/GameplayWebsocketFacade.java
@@ -1,4 +1,28 @@
 package client;
 
 public class GameplayWebsocketFacade {
+
+    public GameplayWebsocketFacade() {
+
+    }
+
+    public void connect() {
+
+    }
+
+    public void sendMove() {
+
+    }
+
+    public void leaveGame() {
+
+    }
+
+    public void resign() {
+
+    }
+
+    public void disconnect() {
+
+    }
 }
