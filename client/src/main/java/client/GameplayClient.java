75718756c5d12520ef93c85b89fd9785bb706e62
Sam Grigg <mrsaaagr@gmail.com>
2026-04-09 12:13:16 -0600

    Should highlight even if no moves

--- client/src/main/java/client/GameplayClient.java
+++ client/src/main/java/client/GameplayClient.java
@@ -67,7 +67,7 @@ public class GameplayClient implements GameplayWebsocketFacade.ServerMessageObse
 
         Collection<ChessPosition> endPositions = new ArrayList<>();
         Collection<ChessMove> validMoves = game.game().validMoves(startPosition);
-        if (validMoves == null || validMoves.isEmpty()) {
+        if (validMoves == null) {
             throw new ResponseException("No valid moves found");
         }
         for (ChessMove move : validMoves) {
